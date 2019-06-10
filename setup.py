2cd670c399a1e5a0497714c80ed747304f427ae9
Yu-Hang "Maxin" Tang <Tang.Maxin@gmail.com>
2019-06-10 11:56:58 -0700

    setting up comprehensive unit test

--- setup.py
+++ setup.py
@@ -1,5 +1,7 @@
-from setuptools import setup, find_packages
 import io
+import sys
+from setuptools import setup, find_packages
+from setuptools.command.test import test as TestCommand
 import graphdot
 
 
@@ -16,22 +18,34 @@ def read(*filenames, **kwargs):
 long_description = read('README.md')
 
 
+class Tox(TestCommand):
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+
+    def run_tests(self):
+        import tox  # import here, cause outside the eggs aren't loaded
+        errcode = tox.cmdline(self.test_args)
+        sys.exit(errcode)
+
+
 setup(
     name='graphdot',
     version=graphdot.__version__,
     url='https://gitlab.com/yhtang/graphdot',
     license='TBD',
     author='Yu-Hang Tang',
-    # tests_require=['pytest'],
+    tests_require=['tox'],
     # install_requires=['Flask>=0.10.1',
     #                 'Flask-SQLAlchemy>=1.0',
     #                 'SQLAlchemy==0.8.2',
     #                 ],
-    # cmdclass={'test': PyTest},
+    cmdclass={'test': Tox},
     author_email='Tang.Maxin@gmail.com',
     description='GPU-accelerated graph similarity measurement library',
     long_description=long_description,
-    packages=['graphdot'],
+    packages=find_packages(exclude='test'),
     # include_package_data=True,
     platforms='any',
     # test_suite='sandman.test.test_sandman',
