7e61bd0abae9f8c4c396154a8c4d4715137b96cc
Yu-Hang "Maxin" Tang <Tang.Maxin@gmail.com>
2019-06-12 16:02:47 -0700

    python/c++ base kernel interoperability module

--- graphdot/marginalized/basekernel/pack.py
+++ graphdot/marginalized/basekernel/pack.py
@@ -1,81 +1,43 @@
 import struct
 
 
-# def pad_one(format):
-#     p = 0
-#     padded_format = ''
-#     align = 1
-#     for s in format:
-#         size = struct.calcsize(s)
-#         align = max(align, size)
-#         while p % size:
-#             padded_format += 'x'
-#             p += 1
-#         padded_format += s
-#         p += size
-#     while p % align:
-#         padded_format += 'x'
-#         p += 1
-#     return padded_format, p, align
-#
-#
-# def pad(format_list):
-#     p = 0
-#     padded_format = ''
-#     align = 1
-#     for format in format_list:
-#         padded_sublayout, subsize, subalign = pad_one(format)
-#         while p % subalign:
-#             padded_format += 'x'
-#             p += 1
-#         padded_format += padded_sublayout
-#         p += subsize
-#         align = max(align, subalign)
-#     while p % align:
-#         padded_format += 'x'
-#         p += 1
-#     return padded_format, p, align
-
-
 def pad(abstract_layout, depth=0):
-    i = 0
+    """
+    Convert nested structures into native C++ layout
+    """
+
     padded_layout = ''
     padded_size = 0
     alignment = 1
+
+    i = 0
     while i < len(abstract_layout):
         print('%s%s  %s' % ('  ' * depth, abstract_layout[i:], padded_layout))
         c = abstract_layout[i]
         if c == '[':
-            step, padded_sublayout, padded_subsize, subalign = pad(abstract_layout[i + 1:], depth + 1)
-            alignment = max(alignment, subalign)
-            print('SUB', step, padded_sublayout, padded_subsize, subalign)
-
-            while padded_size % subalign:
-                padded_layout += 'x'
-                padded_size += 1
-            padded_layout += padded_sublayout
-            padded_size += padded_subsize
-
+            step, sublayout = pad(abstract_layout[i+1:], depth + 1)
+            alignment = max(alignment, sublayout['align'])
+            padding = (sublayout['align'] - padded_size) % sublayout['align']
+            padded_layout += 'x' * padding + sublayout['layout']
+            padded_size += padding + sublayout['size']
             i += step + 1
         elif c == ']':
-            # return i + 1
-            return i + 1, padded_layout, padded_size, alignment
+            return i + 1, {'layout': padded_layout,
+                           'size': padded_size,
+                           'align': alignment}
         else:
             c_size = struct.calcsize(c)
             alignment = max(alignment, c_size)
-            while padded_size % c_size:
-                padded_layout += 'x'
-                padded_size += 1
-            padded_layout += c
-            padded_size += c_size
-
+            padding = (c_size - padded_size) % c_size
+            padded_layout += 'x' * padding + c
+            padded_size += padding + c_size
             i += 1
-    while padded_size % alignment:
-        padded_layout += 'x'
-        padded_size += 1
-    print('Final align', alignment)
-    print('Final size', padded_size)
-    print('Final spec', padded_layout)
+
+    padding = (alignment - padded_size) % alignment
+    padded_layout += 'x' * padding
+    padded_size += padding
+
+    return padded_layout, alignment
 
 
 if __name__ == '__main__':
@@ -88,4 +50,4 @@ if __name__ == '__main__':
     #     print(f, p, a, struct.calcsize(f))
 
     # pad_recursive('[[f][cc]]')
-    pad('[[ch][fdh]]')
+    print(pad('[[ch][fdh]]'))
