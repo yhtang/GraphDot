edb48e63a06d49d2ac8af8950b769f9178fd81f3
Yu-Hang "Maxin" Tang <Tang.Maxin@gmail.com>
2019-06-11 17:09:11 -0700

    consolidated unit test for basekernels

--- test/marginalized/basekernel/test_kernel_theta.py
+++
@@ -1,8 +0,0 @@
-
-@pytest.mark.parametrize('k1', kernels)
-@pytest.mark.parametrize('k2', kernels)
-def test_tensor_product_theta(k1, k2):
-
-
-@pytest.mark.parametrize('kernel', kernels)
-def test_convolution_theta(kernel):
