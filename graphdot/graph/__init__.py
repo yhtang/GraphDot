e4a8556d11861c04440e0434c442c882fecfe67f
Yu-Hang "Maxin" Tang <Tang.Maxin@gmail.com>
2019-06-13 16:12:34 -0700

    refactor and add in new components

---
+++ graphdot/graph/__init__.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+This package defines the Graph container class of graphdot and converts from
+popular graph libraries.
+"""
---
+++ graphdot/graph/adaptors.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+This module defines various adaptor functions to convert graphs into GraphDot's
+own container
+"""
