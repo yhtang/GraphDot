dcc5d5f15846322662efbcb5fad45b934cbb551f
Yu-Hang "Maxin" Tang <Tang.Maxin@gmail.com>
2019-06-25 10:20:52 -0700

    cpp code reorg

---
+++ graphdot/cpp/misc/numpy_type.h
@@ -0,0 +1,27 @@
+#ifndef GRAPHDOT_MISC_NPTYPE_H
+#define GRAPHDOT_MISC_NPTYPE_H
+#include <cstdint>
+
+namespace graphdot {
+
+namespace numpy_type {
+    using bool_ = bool;
+    using int_ = long;
+    using intc = int;
+    using intp = std::size_t;
+    using uint8 = std::uint8_t;
+    using uint16 = std::uint16_t;
+    using uint32 = std::uint32_t;
+    using uint64 = std::uint64_t;
+    using int8 = std::int8_t;
+    using int16 = std::int16_t;
+    using int32 = std::int32_t;
+    using int64 = std::int64_t;
+    using float_ = double;
+    using float32 = float;
+    using float64 = double;
+}
+
+}
+
+#endif
