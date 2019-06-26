8d9232f0f9976d3c9385a13401d6b0687ce110e7
Yu-Hang "Maxin" Tang <Tang.Maxin@gmail.com>
2019-06-26 16:35:18 -0700

    python version of the scratch class

---
+++ graphdot/marginalized/scratch.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import numpy as np
+from pycuda.gpuarray import empty
+from graphdot.codegen.interop import cpptype
+
+
+@cpptype([('ptr', np.uintp), ('capacity', np.int64)])
+class BlockScratch:
+    def __init__(self, capacity):
+        self.capacity = ((capacity + 15) // 16) * 16
+        self.buffer = empty(self.capacity * 4, np.float32)
+
+    @property
+    def ptr(self):
+        return self.buffer.ptr
+
+
+if __name__ == '__main__':
+
+    import pycuda.autoinit
+
+    scratch = BlockScratch(1024)
+
+    print(scratch.state)
