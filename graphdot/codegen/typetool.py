a26ef66ff78db53c1dc2c73d31f4c29f31377fa6
Yu-Hang "Maxin" Tang <Tang.Maxin@gmail.com>
2019-06-26 20:12:00 -0700

    refactor

--- graphdot/codegen/typetool.py
+++ graphdot/codegen/typetool.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+from graphdot.codegen import Template
 import numpy as np
 
 
@@ -31,6 +32,31 @@ def cpptype(dtype):
         return Class
 
     return decor
+
+
+def decltype(type):
+    type = np.dtype(type, align=True)  # convert np.float32 etc. to dtype
+    if type.names is not None:
+        return Template(r'''struct{${members;};}''').render(
+            members=['{} {}'.format(decltype(t), v)
+                     for v, (t, offset) in type.fields.items()])
+    # elif type.subdtype is not None:
+    #     return Template(r'''${type} ${name}${dim}''').render(
+    #         type=type.name, name=
+    #     )
+    else:
+        return str(type.name)
+
+
+def rowtype(df, pack=True):
+    if pack is True:
+        order = np.argsort([-df.dtypes[key].itemsize for key in df.columns])
+    else:
+        order = np.arange(len(df.columns))
+    packed_attributes = [df.columns[i] for i in order]
+    packed_dtype = np.dtype([(key, df.dtypes[key].newbyteorder('='))
+                             for key in packed_attributes], align=True)
+    return packed_dtype
 
 
 if __name__ == '__main__':
