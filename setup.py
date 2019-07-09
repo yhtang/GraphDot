82f9e9df1f957ebd4170b34de5cc2dd069f8af6b
Yu-Hang "Maxin" Tang <Tang.Maxin@gmail.com>
2019-07-09 15:58:42 -0700

    update setup resource config, add CPU gold bench to test case

--- setup.py
+++ setup.py
@@ -49,7 +49,7 @@ setup(
     long_description=long_description,
     packages=find_packages(exclude='test'),
     package_data={
-        'graphdot': ['*/*.h', '*/*/*.h', '*/*.cu'],
+        'graphdot': ['*/*.h', '*/*/*.h', '*/*/*.cu'],
     },
     # include_package_data=True,
     platforms='any',
