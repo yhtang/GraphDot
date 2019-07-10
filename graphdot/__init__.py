73c26f2791354f1830ff6c915fc1a4d933fe8db3
Yu-Hang "Maxin" Tang <Tang.Maxin@gmail.com>
2019-07-10 11:38:11 -0700

    minor update on __license__

--- graphdot/__init__.py
+++ graphdot/__init__.py
@@ -22,6 +22,6 @@ __all__ = ['Graph']
 __version__ = '0.1a1'
 __author__ = 'Yu-Hang "Maxin" Tang, Oguz Selvitopi, Doru Popovici'
 __copyright__ = 'Copyright 2019'
-__license__ = 'TBD'
 __maintainer__ = 'Yu-Hang "Maxin" Tang'
 __email__ = 'Tang.Maxin@gmail.com'
+__license__ = 'see LICENSE file'
