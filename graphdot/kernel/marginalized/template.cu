6263b1a3ac8ec94b1223ebb1fde7151b033e3921
Yu-Hang "Maxin" Tang <Tang.Maxin@gmail.com>
2019-07-29 17:13:03 -0700

    GPU kernel now return nodal similarity matrices and let the CPU side handle summation

--- graphdot/kernel/marginalized/template.cu
+++ graphdot/kernel/marginalized/template.cu
@@ -20,11 +20,10 @@ __constant__ char shmem_bytes_per_warp[solver_t::shmem_bytes_per_warp];
 extern "C" {
     __global__ void graph_kernel_solver(
         graph_t const * graphs,
-        scratch_t   *   scratch,
-        job_t     *     jobs,
+        scratch_t     * scratch,
+        job_t         * jobs,
         unsigned int  * i_job_global,
         const unsigned  n_jobs,
-        const float     s,
         const float     q
     ) {
         extern __shared__ char shmem[];
@@ -36,12 +35,11 @@ extern "C" {
 
             if (i_job >= n_jobs) break;
 
-            auto out = solver_t::compute<node_kernel, edge_kernel> (
-                           graphs[ jobs[i_job].in.i ],
-                           graphs[ jobs[i_job].in.j ],
-                           scratch[ blockIdx.x ],
-                           shmem, s, q);
-            if (threadIdx.x == 0) jobs[i_job] = out;
+            solver_t::compute<node_kernel, edge_kernel> (
+                graphs[ jobs[i_job].i ],
+                graphs[ jobs[i_job].j ],
+                scratch[ blockIdx.x ],
+                shmem, q, jobs[i_job].vr);
             __syncthreads();
         }
     }
