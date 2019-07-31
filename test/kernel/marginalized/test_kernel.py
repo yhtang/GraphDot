5e350a846bad0fe5b97bf98fce088584210d454c
Yu-Hang "Maxin" Tang <Tang.Maxin@gmail.com>
2019-07-31 11:20:34 -0700

    updating unit test for marginalized graph kernel

--- test/kernel/marginalized/test_kernel.py
+++ test/kernel/marginalized/test_kernel.py
@@ -11,60 +11,7 @@ from graphdot.kernel.marginalized.basekernel import SquareExponential
 from graphdot.kernel.marginalized.basekernel import TensorProduct
 
 
-unlabeled_graph1 = nx.Graph(title='U1')
-unlabeled_graph1.add_node(0)
-unlabeled_graph1.add_node(1)
-unlabeled_graph1.add_node(2)
-unlabeled_graph1.add_edge(0, 1)
-unlabeled_graph1.add_edge(0, 2)
-
-unlabeled_graph2 = nx.Graph(title='U2')
-unlabeled_graph2.add_node(0)
-unlabeled_graph2.add_node(1)
-unlabeled_graph2.add_node(2)
-unlabeled_graph2.add_edge(0, 1)
-unlabeled_graph2.add_edge(0, 2)
-unlabeled_graph2.add_edge(1, 2)
-
-vanilla_graph = nx.Graph(title='vanilla')
-vanilla_graph.add_node('a', type=0)
-vanilla_graph.add_node('b', type=0)
-vanilla_graph.add_edge('a', 'b', weight=1.0)
-
-
-class Hybrid:
-    NONE = 0
-    SP = 1
-    SP2 = 2
-    SP3 = 3
-
-
-labeled_graph1 = nx.Graph(title='H2O')
-labeled_graph1.add_node('O1', hybridization=Hybrid.SP2, charge=1)
-labeled_graph1.add_node('H1', hybridization=Hybrid.SP3, charge=-1)
-labeled_graph1.add_node('H2', hybridization=Hybrid.SP, charge=2)
-labeled_graph1.add_edge('O1', 'H1', order=1, length=0.5)
-labeled_graph1.add_edge('O1', 'H2', order=2, length=1.0)
-
-labeled_graph2 = nx.Graph(title='H2')
-labeled_graph2.add_node('H1', hybridization=Hybrid.SP, charge=1)
-labeled_graph2.add_node('H2', hybridization=Hybrid.SP, charge=1)
-labeled_graph2.add_edge('H1', 'H2', order=2, length=1.0)
-
-weighted_graph1 = nx.Graph(title='H2O')
-weighted_graph1.add_node('O1', hybridization=Hybrid.SP2, charge=1)
-weighted_graph1.add_node('H1', hybridization=Hybrid.SP3, charge=-1)
-weighted_graph1.add_node('H2', hybridization=Hybrid.SP, charge=2)
-weighted_graph1.add_edge('O1', 'H1', order=1, length=0.5, w=1.0)
-weighted_graph1.add_edge('O1', 'H2', order=2, length=1.0, w=2.0)
-
-weighted_graph2 = nx.Graph(title='H2')
-weighted_graph2.add_node('H1', hybridization=Hybrid.SP, charge=1)
-weighted_graph2.add_node('H2', hybridization=Hybrid.SP, charge=1)
-weighted_graph2.add_edge('H1', 'H2', order=2, length=1.0, w=3.0)
-
-
-def MLGK(G, knode, kedge, q):
+def MLGK(G, knode, kedge, q, nodal=False):
     N = len(G.nodes)
     D = np.zeros(N)
     A = np.zeros((N, N))
@@ -106,14 +53,65 @@ def MLGK(G, knode, kedge, q):
     linsys = np.diag(Dx / Vx) - Ax * Ex
     solution = np.linalg.solve(linsys, rhs)
 
-    return solution.sum()
+    if nodal is True:
+        return solution.reshape(N, -1)
+    else:
+        return solution.sum()
+
+
+unlabeled_graph1 = nx.Graph(title='U1')
+unlabeled_graph1.add_node(0)
+unlabeled_graph1.add_node(1)
+unlabeled_graph1.add_node(2)
+unlabeled_graph1.add_edge(0, 1)
+unlabeled_graph1.add_edge(0, 2)
+
+unlabeled_graph2 = nx.Graph(title='U2')
+unlabeled_graph2.add_node(0)
+unlabeled_graph2.add_node(1)
+unlabeled_graph2.add_node(2)
+unlabeled_graph2.add_edge(0, 1)
+unlabeled_graph2.add_edge(0, 2)
+unlabeled_graph2.add_edge(1, 2)
+
+
+class Hybrid:
+    NONE = 0
+    SP = 1
+    SP2 = 2
+    SP3 = 3
+
+
+labeled_graph1 = nx.Graph(title='H2O')
+labeled_graph1.add_node('O1', hybridization=Hybrid.SP2, charge=1)
+labeled_graph1.add_node('H1', hybridization=Hybrid.SP3, charge=-1)
+labeled_graph1.add_node('H2', hybridization=Hybrid.SP, charge=2)
+labeled_graph1.add_edge('O1', 'H1', order=1, length=0.5)
+labeled_graph1.add_edge('O1', 'H2', order=2, length=1.0)
+
+labeled_graph2 = nx.Graph(title='H2')
+labeled_graph2.add_node('H1', hybridization=Hybrid.SP, charge=1)
+labeled_graph2.add_node('H2', hybridization=Hybrid.SP, charge=1)
+labeled_graph2.add_edge('H1', 'H2', order=2, length=1.0)
+
+weighted_graph1 = nx.Graph(title='H2O')
+weighted_graph1.add_node('O1', hybridization=Hybrid.SP2, charge=1)
+weighted_graph1.add_node('H1', hybridization=Hybrid.SP3, charge=-1)
+weighted_graph1.add_node('H2', hybridization=Hybrid.SP, charge=2)
+weighted_graph1.add_edge('O1', 'H1', order=1, length=0.5, w=1.0)
+weighted_graph1.add_edge('O1', 'H2', order=2, length=1.0, w=2.0)
+
+weighted_graph2 = nx.Graph(title='H2')
+weighted_graph2.add_node('H1', hybridization=Hybrid.SP, charge=1)
+weighted_graph2.add_node('H2', hybridization=Hybrid.SP, charge=1)
+weighted_graph2.add_edge('H1', 'H2', order=2, length=1.0, w=3.0)
 
 
 def test_mlgk_typecheck():
     node_kernel = Constant(1.0)
     edge_kernel = Constant(1.0)
     mlgk = MarginalizedGraphKernel(node_kernel, edge_kernel, q=0.5)
-    G = [Graph.from_networkx(vanilla_graph),
+    G = [Graph.from_networkx(unlabeled_graph1),
          Graph.from_networkx(labeled_graph1),
          Graph.from_networkx(weighted_graph1, weight='w')]
 
@@ -131,79 +129,51 @@ def test_mlgk_typecheck():
         mlgk([G[2], G[1]])
 
 
-def test_mlgk_unlabeled():
-    u1 = Graph.from_networkx(unlabeled_graph1)
-    u2 = Graph.from_networkx(unlabeled_graph1)
+@pytest.mark.parametrize('case', {
+    'unlabeled': {
+        'graphs': [Graph.from_networkx(unlabeled_graph1),
+                   Graph.from_networkx(unlabeled_graph2)],
+        'knode': Constant(1.0),
+        'kedge': Constant(1.0),
+        'q': [0.01, 0.05, 0.1, 0.5]
+    },
+    'labeled': {
+        'graphs': [Graph.from_networkx(labeled_graph1),
+                   Graph.from_networkx(labeled_graph2)],
+        'knode': TensorProduct(hybridization=KroneckerDelta(0.3, 1.0),
+                               charge=SquareExponential(1.0)),
+        'kedge': TensorProduct(order=KroneckerDelta(0.3, 1.0),
+                               length=SquareExponential(0.05)),
+        'q': [0.01, 0.05, 0.1, 0.5]
+    },
+    'weighted': {
+        'graphs': [Graph.from_networkx(weighted_graph1, weight='w'),
+                   Graph.from_networkx(weighted_graph2, weight='w')],
+        'knode': TensorProduct(hybridization=KroneckerDelta(0.3, 1.0),
+                               charge=SquareExponential(1.0)),
+        'kedge': TensorProduct(order=KroneckerDelta(0.3, 1.0),
+                               length=SquareExponential(0.05)),
+        'q': [0.01, 0.05, 0.1, 0.5]
+    },
+})
+def test_mlgk(case):
 
-    q = 0.5
-    node_kernel = Constant(1.0)
-    edge_kernel = Constant(1.0)
-    mlgk = MarginalizedGraphKernel(node_kernel, edge_kernel, q=q)
+    G = [Graph.from_networkx(g) for g in case['graphs']]
+    knode = case['knode']
+    kedge = case['kedge']
+    for q in case['q']:
+        mlgk = MarginalizedGraphKernel(knode, kedge, q=q)
+        R = mlgk(G)
+        d = np.diag(R)**-0.5
+        K = np.diag(d).dot(R).dot(np.diag(d))
 
-    dot = mlgk([u1, u2])
-    d = np.diag(dot)**-0.5
-    dot = np.diag(d).dot(dot).dot(np.diag(d))
+        assert(K.shape == (len(G), len(G)))
+        assert(np.count_nonzero(K - K.T) == 0)
+        assert(R[0, 0] == pytest.approx(MLGK(G[0], knode, kedge, q)))
+        assert(R[1, 1] == pytest.approx(MLGK(G[1], knode, kedge, q)))
+        assert(K[0, 0] == pytest.approx(1, 1e-7))
+        assert(K[1, 1] == pytest.approx(1, 1e-7))
 
-    assert(dot.shape == (2, 2))
-    for d in dot.ravel():
-        assert(d == pytest.approx(1, 1e-7))
-
-
-def test_mlgk_vanilla():
-    dfg = Graph.from_networkx(vanilla_graph, weight='weight')
-
-    q = 0.5
-    node_kernel = TensorProduct(type=KroneckerDelta(1.0, 1.0))
-    edge_kernel = Constant(1.0)
-    mlgk = MarginalizedGraphKernel(node_kernel, edge_kernel, q=q)
-
-    dot = mlgk([dfg])
-    gold = MLGK(dfg, node_kernel, edge_kernel, q)
-
-    assert(dot.shape == (1, 1))
-    assert(dot.item() == pytest.approx(gold))
-
-
-def test_mlgk_labeled():
-    G = [Graph.from_networkx(g) for g in [labeled_graph1, labeled_graph2]]
-
-    q = 0.1
-    node_kernel = TensorProduct(hybridization=KroneckerDelta(0.3, 1.0),
-                                charge=SquareExponential(1.0))
-
-    edge_kernel = TensorProduct(order=KroneckerDelta(0.3, 1.0),
-                                length=SquareExponential(0.05))
-    mlgk = MarginalizedGraphKernel(node_kernel, edge_kernel, q=q)
-    R = mlgk(G)
-
-    assert(R.shape == (2, 2))
-    assert(np.count_nonzero(R - R.T) == 0)
-    assert(R[0, 0] == pytest.approx(MLGK(G[0], node_kernel, edge_kernel, q)))
-    assert(R[1, 1] == pytest.approx(MLGK(G[1], node_kernel, edge_kernel, q)))
-
-
-def test_mlgk_weighted():
-    G = [Graph.from_networkx(g, weight='w') for g in [weighted_graph1,
-                                                      weighted_graph2]]
-
-    q = 0.1
-    node_kernel = TensorProduct(hybridization=KroneckerDelta(0.3, 1.0),
-                                charge=SquareExponential(1.0))
-
-    edge_kernel = TensorProduct(order=KroneckerDelta(0.3, 1.0),
-                                length=SquareExponential(0.05))
-    mlgk = MarginalizedGraphKernel(node_kernel, edge_kernel, q=q)
-
-    R = mlgk(G)
-
-    assert(R.shape == (2, 2))
-    assert(np.count_nonzero(R - R.T) == 0)
-    assert(R[0, 0] == pytest.approx(MLGK(G[0], node_kernel, edge_kernel, q)))
-    assert(R[1, 1] == pytest.approx(MLGK(G[1], node_kernel, edge_kernel, q)))
-
-    R = mlgk(G, nodal=True)
-    n = np.sum([len(g.nodes) for g in G])
-    assert(R.shape == (n, n))
 
 # def test_mlgk_large():
 #     g = nx.Graph()
