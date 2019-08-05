e815363bf34af9a2e6d294a04e42b4261871a8ef
Yu-Hang "Maxin" Tang <Tang.Maxin@gmail.com>
2019-08-05 16:02:28 -0700

    test nodal similarity and starting probability for the molecular kernel

--- test/kernel/test_molecular.py
+++ test/kernel/test_molecular.py
@@ -21,3 +21,24 @@ def test_molecular_kernel():
     assert(R.shape == (3, 3))
     for i in range(len(molecules)):
         assert(K[i, i] == pytest.approx(1, 1e-6))
+
+    R_nodal = kernel(graphs, nodal=True)
+    D_nodal = np.diag(np.diag(R_nodal)**-0.5)
+    K_nodal = D_nodal.dot(R_nodal).dot(D_nodal)
+
+    natoms = np.sum([len(m) for m in molecules])
+    assert(R_nodal.shape == (natoms, natoms))
+    for i in range(natoms):
+        assert(K_nodal[i, i] == pytest.approx(1, 1e-6))
+
+    kernel_nocarbon = Tang2019MolecularKernel(
+        starting_probability=lambda n: 0.0 if n[1]['element'] == 6 else 1.0)
+
+    R_nocarbon_nodal = kernel_nocarbon(graphs, nodal=True)
+    k = 0
+    for i, m in enumerate(molecules):
+        for j, a in enumerate(m):
+            if a.symbol == 'C':
+                assert(R_nocarbon_nodal[k, :].sum() == 0)
+                assert(R_nocarbon_nodal[:, k].sum() == 0)
+            k += 1
