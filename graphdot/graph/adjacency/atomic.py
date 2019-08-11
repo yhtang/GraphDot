15bb22526331d5258ca7c80cddb3f32cfe75ebd0
Yin-Jia Zhang <zyinjia@gmail.com>
2019-08-10 22:41:01 -0700

    added AtomicAdjacency

--- graphdot/graph/adjacency/atomic.py
+++ graphdot/graph/adjacency/atomic.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+import re
 import numpy as np
+from mendeleev import Element
 from graphdot.graph.adjacency.euclidean import Tent
 
 
@@ -19,3 +21,57 @@ class SimpleTentAtomicAdjacency:
     @property
     def cutoff(self):
         return self.h * 3
+
+
+class AtomicAdjacency:
+    """Converts interatomic distances into edge weights
+
+    Parameters
+    ----------
+    zoom: float
+        A zooming factor to be multiplied with inferred length scales.
+    shape: str or function
+        A 1D weight real-valued function that converts distance to weight.
+    images: None or list of 3D vectors
+        Displacement vector of periodic images to be checked for finding
+        the closest neighbors in a crystal.
+    """
+
+    def __init__(self, zoom=1, shape='tent1', images=None):
+        self.zoom = zoom
+        self.images = images
+        if isinstance(shape, str):
+            m = re.match('tent([1-9]\d*)', shape)
+            if m:
+                self.shape= Tent(ord = int(m.group(1)))
+            else:
+                raise ValueError('Invalid shape: {}'.format(shape))
+        else:
+            self.shape = shape
+
+
+    def __call__(self, atom1, atom2):
+        """compute adjacency between atoms
+
+        Parameters
+        ----------
+        atom1: :py:class:`ase.Atom` instance
+            An atom object with properties such as `position`, `number`, `index`, etc.
+        atom2: :py:class:`ase.Atom` instance
+            Same as `atom1`
+
+        Returns
+        -------
+        (float, float):
+            A non-negative weight and a min distance
+        """
+        dx = atom1.position - atom2.position
+        dr = np.linalg.norm(dx + self.images, axis=1)
+        imin = np.argmin(dr)
+        r1 = Element(atom1.number).covalent_radius
+        r2 = Element(atom2.number).covalent_radius
+        weight = self.shape(
+            dr[imin],
+            self.zoom * np.sqrt(r1 * r2)
+        )
+        return weight, dr[imin]
--- graphdot/graph/adjacency/euclidean.py
+++ graphdot/graph/adjacency/euclidean.py
@@ -7,18 +7,15 @@ import numpy as np
 
 
 class Gaussian:
-    def __init__(self, length_scale):
-        self.length_scale = length_scale
-
-    def __call__(self, d):
-        return np.exp(-0.5 * (d / self.length_scale)**2)
+    def __call__(self, d, length_scale):
+        return np.exp(-0.5 * (d / length_scale)**2)
 
 
 class Tent:
-    def __init__(self, cut, ord):
-        self.cut = cut
+    def __init__(self, ord):
         self.ord = ord
 
-    def __call__(self, d):
-        s = 1 - d / self.cut
-        return s**self.ord if s >= 0 else 0
+    def __call__(self, d, length_scale):
+        cutoff = length_scale * 3
+        s = 1 - d / cutoff
+        return s ** self.ord if s >= 0 else 0
