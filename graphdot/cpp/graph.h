80fce0cc9438356bb7c7258dad23ce8e5568e6ee
Yu-Hang "Maxin" Tang <Tang.Maxin@gmail.com>
2019-10-21 16:47:49 -0700

    new compact graph format on GPU

---
+++ graphdot/cpp/graph.h
@@ -0,0 +1,37 @@
+#ifndef GRAPHDOT_GRAPH_H_
+#define GRAPHDOT_GRAPH_H_
+
+#include <cstdint>
+
+namespace graphdot {
+
+template<class Node, class Edge> struct graph_t {
+
+    using deg_t  = float;
+    using node_t = Node;
+    using edge_t = Edge;
+    using octile_t = struct {
+        edge_t * elements;
+        union {
+            std::uint64_t nzmask;            // column major
+            std::uint32_t nzmask_halves[2];  // one byte for each column
+            std::uint8_t  nzmask_bytes[8];   // one byte for each column
+        };
+        int upper, left;
+    };
+
+    constexpr static float eps = 1e-14;
+
+    int n_node, n_octile;
+    deg_t   *  degree;
+    node_t  *  vertex;
+    octile_t * octile;
+
+    constexpr __inline__ __host__ __device__ int padded_size() const {
+        return (n_node + 7) & ~7U;
+    }
+};
+
+}
+
+#endif
