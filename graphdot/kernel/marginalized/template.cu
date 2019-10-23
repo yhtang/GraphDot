f19ffd516d271698b9121a9442a38115707d2259
Yu-Hang "Maxin" Tang <Tang.Maxin@gmail.com>
2019-10-23 13:07:16 -0700

    fixed rebase issue

--- graphdot/kernel/marginalized/template.cu
+++ graphdot/kernel/marginalized/template.cu
@@ -10,7 +10,7 @@ ${edge_kernel}
 using node_t = ${node_t};
 using edge_t = ${edge_t};
 
-using graph_t   = graphdot::marginalized::graph_t<node_t, edge_t>;
+using graph_t   = graphdot::graph_t<node_t, edge_t>;
 using scratch_t = graphdot::marginalized::pcg_scratch_t;
 using job_t     = graphdot::marginalized::job_t;
 using solver_t  = graphdot::marginalized::labeled_compact_block_dynsched_pcg<graph_t>;
