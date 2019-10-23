82a006051ca27c382d026075bf6fcfcb5f138a35
Yu-Hang "Maxin" Tang <Tang.Maxin@gmail.com>
2019-10-22 22:31:24 -0700

    implemented a Timer class for timing convenience

--- graphdot/util/__init__.py
+++ graphdot/util/__init__.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+import time
+from collections import OrderedDict
 
 
 def add_classmethod(cls, override=False):
@@ -12,3 +14,33 @@ def add_classmethod(cls, override=False):
         setattr(cls, func.__name__, clsm)
         return clsm
     return decorate
+
+
+class Timer:
+    def __init__(self):
+        self.reset()
+
+    def reset(self):
+        self.t = OrderedDict()
+        self.dt = OrderedDict()
+
+    def tic(self, tag):
+        self.t[tag] = time.perf_counter()
+
+    def toc(self, tag):
+        self.dt[tag] = time.perf_counter() - self.t[tag]
+        del self.t[tag]
+
+    def report(self, unit='s'):
+        if unit == 's':
+            scale = 1.0
+        elif unit == 'ms':
+            scale = 1e3
+        elif unit == 'us':
+            scale = 1e6
+        elif unit == 'ns':
+            scale = 1e9
+        else:
+            raise ValueError('Unknown unit %s' % unit)
+        for tag, dt in self.dt.items():
+            print('%9.1f %s on %s' % (dt * scale, unit, tag))
