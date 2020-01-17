326be94a735d3ebfb453dab1a0363c680d25a35a
Yu-Hang "Maxin" Tang <Tang.Maxin@gmail.com>
2020-01-16 23:24:47 -0800

    Bumped version number.

--- graphdot/__init__.py
+++ graphdot/__init__.py
@@ -5,7 +5,7 @@ from .graph import Graph
 
 __all__ = ['Graph']
 
-__version__ = '0.3.0'
+__version__ = '0.3.1'
 __author__ = 'Yu-Hang "Maxin" Tang, Oguz Selvitopi'
 __maintainer__ = 'Yu-Hang "Maxin" Tang'
 __email__ = 'Tang.Maxin@gmail.com'
