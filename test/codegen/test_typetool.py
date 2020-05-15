492a182a6740f4cff23cd7a8d12db752868d4544
Yu-Hang "Maxin" Tang <Tang.Maxin@gmail.com>
2020-05-15 15:30:19 -0700

    updating unit tests

--- test/codegen/test_typetool.py
+++ test/codegen/test_typetool.py
@@ -5,7 +5,7 @@ from copy import copy
 import numpy as np
 import pytest
 import exrex
-from graphdot.codegen.typetool import cpptype, decltype, rowtype
+from graphdot.codegen.typetool import cpptype, decltype
 from graphdot.minipandas import DataFrame
 
 
@@ -207,30 +207,3 @@ def test_decltype_order():
             cnext = '%s;' % decltype(type.fields[next][0], next)
             assert(search(cprev, cstr).start() <=
                    search(cnext, cstr).start())
-
-
-rowtype_cases = [
-    # empty dataframe
-    (DataFrame([]), np.dtype([], align=True)),
-    # different types of columns
-    (DataFrame({'A': [1.5, 42.0],
-                'B': [-1, 32768],
-                'C': [False, True]}),
-     np.dtype([('A', np.float64),
-               ('B', np.int64),
-               ('C', np.bool_)], align=True)),
-    # small-big-small layout optimization
-    (DataFrame({'A': [True, False],
-                'B': [-1, 1],
-                'C': [False, True]}),
-     np.dtype([('A', np.bool_),
-               ('B', np.int64),
-               ('C', np.bool_)], align=True)),
-]
-
-
-@pytest.mark.parametrize('case', rowtype_cases)
-def test_rowtype(case):
-    df, dtype = case
-    # assert(rowtype(df, pack=False) == dtype)
-    assert(rowtype(df, pack=False).itemsize >= rowtype(df, pack=True).itemsize)
