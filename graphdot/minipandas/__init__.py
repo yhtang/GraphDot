4f00a5680ff0aecb2e56fd9b867fcfe7cf00fd0d
Yu-Hang "Maxin" Tang <Tang.Maxin@gmail.com>
2020-05-14 20:38:08 -0700

    data frame functionality improvements.

--- graphdot/minipandas/__init__.py
+++ graphdot/minipandas/__init__.py
@@ -3,13 +3,11 @@
 from collections import namedtuple
 import numpy as np
 import pandas as pd
+from graphdot.codegen.typetool import common_min_type
 
 
 def _as_1darray(array):
-    _1darray = np.empty(
-        len(array),
-        dtype=np.find_common_type([], map(type, array))
-    )
+    _1darray = np.empty(len(array), dtype=common_min_type(array))
     _1darray[:] = array
     return _1darray
 
@@ -22,31 +20,39 @@ class Series(np.ndarray):
 
 class DataFrame:
     def __init__(self, data=None):
-        self.columns = {}
+        self._data = {}
 
         if isinstance(data, dict):
             for key, value in data.items():
                 self[key] = value
 
     def __getitem__(self, key):
-        return self.columns[key]
+        return self._data[key]
 
     def __setitem__(self, key, value):
         if not isinstance(value, np.ndarray):
             value = _as_1darray(value)
-        self.columns[key] = value.view(Series)
+        self._data[key] = value.view(Series)
 
     def __repr__(self):
-        return repr(self.columns)
+        return repr(self._data)
 
     def __len__(self):
-        return max([0] + [len(array) for array in self.columns.values()])
+        return max([0] + [len(array) for array in self._data.values()])
 
     def __contains__(self, item):
-        return item in self.columns
+        return item in self._data
+
+    def __iter__(self):
+        for key in self._data:
+            yield key
+
+    @property
+    def columns(self):
+        yield from self._data.keys()
 
     def rows(self):
-        visible = [key for key in self.columns if not key.startswith('!')]
+        visible = [key for key in self._data if not key.startswith('!')]
 
         class RowTuple(namedtuple('RowTuple', visible)):
             def __getitem__(self, key):
@@ -56,7 +62,7 @@ class DataFrame:
                     return super().__getitem__(key)
 
         for i in range(len(self)):
-            yield RowTuple(*[self.columns[key][i] for key in visible])
+            yield RowTuple(*[self._data[key][i] for key in visible])
 
     def itertuples(self):
         yield from self.rows()
@@ -65,12 +71,16 @@ class DataFrame:
         yield from enumerate(self.rows())
 
     def to_pandas(self):
-        return pd.DataFrame(self.columns)
+        return pd.DataFrame(self._data)
 
     def copy(self, deep=False):
         if deep:
             return self.__class__({
-                key: np.copy(value) for key, value in self.columns.items()
+                key: np.copy(value) for key, value in self._data.items()
             })
         else:
-            return self.__class__(self.columns)
+            return self.__class__(self._data)
+
+    def drop(self, keys):
+        for key in keys:
+            del self._data[key]
