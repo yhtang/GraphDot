815be17b0c986536249915ee7ee011e03a4ef2c3
Yu-Hang "Maxin" Tang <Tang.Maxin@gmail.com>
2020-05-21 22:41:25 -0700

    Convolution kernel done.

--- graphdot/cpp/frozen_array.h
+++ graphdot/cpp/frozen_array.h
@@ -1,5 +1,5 @@
-#ifndef GRAPHDOT_CONV_H_
-#define GRAPHDOT_CONV_H_
+#ifndef GRAPHDOT_FROZEN_ARRAY_H_
+#define GRAPHDOT_FROZEN_ARRAY_H_
 
 #include <type_traits>
 #include "numpy_type.h"
@@ -15,29 +15,29 @@ struct frozen_array {
     using const_reference_type = std::add_lvalue_reference_t<std::add_const_t<element_type>>;
     using pointer_type = std::add_pointer_t<element_type>;
     using size_type = numpy_type::int32;
-    pointer_type * _data = nullptr;
+    pointer_type _data = nullptr;
     size_type size = 0;
 
     struct const_iterator {
-        pointer_type * _ptr;
+        pointer_type _ptr;
 
-        void operator ++ () {++_ptr;}
+        __host__ __device__ void operator ++ () {++_ptr;}
 
-        const_reference_type operator * () {
+        __host__ __device__ const_reference_type operator * () {
             return *_ptr;
         }
 
-        bool operator != (const_iterator const &other) {
+        __host__ __device__ bool operator != (const_iterator const &other) {
             return _ptr != other._ptr;
         }
     };
 
-    const_iterator begin() const {
-        return const_iterator {_ptr};
+    __host__ __device__ const_iterator begin() const {
+        return const_iterator {_data};
     }
 
-    const_iterator end() const {
-        return const_iterator {_ptr + size};
+    __host__ __device__ const_iterator end() const {
+        return const_iterator {_data + size};
     }
 };
 
