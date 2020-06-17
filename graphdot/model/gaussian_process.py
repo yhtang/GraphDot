2036cc8691f58c7da4a47a67598611b128a9581a
Yu-Hang "Maxin" Tang <Tang.Maxin@gmail.com>
2020-06-17 01:33:24 -0700

    Unit test for GPR hyperparameter optimization.

--- graphdot/model/gaussian_process.py
+++ graphdot/model/gaussian_process.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import numpy as np
+from scipy.optimize import minimize
 from ._chol_solver import CholSolver
 
 
@@ -9,20 +10,26 @@ class GaussianProcessRegressor:
         self.kernel = kernel
         self.alpha = alpha
         self.optimizer = optimizer
+        if optimizer is True:
+            self.optimizer = 'L-BFGS-B'
         self.normalize_y = normalize_y
 
     def fit(self, X, y):
+        # set X
         self.X = X
+        # set y
+        if self.normalize_y is True:
+            self.y_mean, self.y_std = np.mean(y), np.std(y)
+            self.y = (y - self.y_mean) / self.y_std
+        else:
+            self.y_mean, self.y_std = 0, 1
+            self.y = np.copy(y)
+        # train model
+        if self.optimizer:
+            self._optimize_hyperparameters(X, y)
         self.K = K = self.kernel(X)
         K.flat[::len(K) + 1] += self.alpha
         self.L = CholSolver(K)
-        if self.normalize_y is True:
-            m, s = np.mean(y), np.std(y)
-            self.y = (y - m) / s
-            self.y_scaler = lambda y0, m=m, s=s: y0 * s + m
-        else:
-            self.y = np.copy(y)
-            self.y_scaler = lambda y0: y0
         self.Ky = self.L(y)
         return self
 
@@ -30,15 +37,15 @@ class GaussianProcessRegressor:
         if not hasattr(self, 'K'):
             raise RuntimeError('Model not trained.')
         Ks = self.kernel(Z, self.X)
-        pred = self.y_scaler(Ks @ self.Ky)
+        pred = (Ks @ self.Ky) * self.y_std + self.y_mean
         if return_std is True:
             Kss = self.kernel.diag(Z)
             std = np.sqrt(np.maximum(Kss - (Ks @ self.L(Kss.T)).diagonal(), 0))
-            return (pred, std)
+            return (pred, std * self.y_std)
         elif return_cov is True:
             Kss = self.kernel(Z)
             cov = np.maximum(Kss - Ks @ self.L(Kss.T), 0)
-            return (pred, cov)
+            return (pred, cov * self.y_std**2)
         else:
             return pred
 
@@ -61,14 +68,35 @@ class GaussianProcessRegressor:
         yKy = self.y @ Ky
         logdet = np.prod(np.linalg.slogdet(K))
         if verbose:
-            print('L %.5f = %.5f + %.5f' % (yKy + logdet, yKy, logdet), theta)
+            print(
+                'L %.5f = %.5f + %.5f' % (yKy + logdet, yKy, logdet),
+                np.exp(theta)
+            )
 
         if eval_gradient is True:
-            D_theta = []
+            D_theta = np.zeros(dK.shape[0])
             for i in range(dK.shape[0]):
                 dk = dK[i, :, :]
-                dt = (L(dk).trace() - Ky @ dk @ Ky) * np.exp(theta[i])
-                D_theta.append(dt)
-            return yKy + logdet, np.array(D_theta)
+                D_theta[i] = (L(dk).trace() - Ky @ dk @ Ky) * np.exp(theta[i])
+            return yKy + logdet, D_theta
         else:
             return yKy + logdet
+
+    def _optimize_hyperparameters(self, X, y):
+        opt = minimize(
+            fun=lambda theta, self=self: self.log_marginal_likelihood(
+                theta, eval_gradient=True, clone_kernel=False
+            ),
+            method=self.optimizer,
+            x0=self.kernel.theta,
+            bounds=self.kernel.bounds,
+            jac=True,
+            tol=1e-3,
+        )
+
+        if opt.success:
+            self.kernel.theta = opt.x
+        else:
+            raise RuntimeError(
+                f'Hyperparameter optimization did not converge, result:\n{opt}'
+            )
--- test/model/test_gaussian_process.py
+++ test/model/test_gaussian_process.py
@@ -54,14 +54,56 @@ def test_gpr_periodic_regression():
             return np.ones_like(X)
 
     kernel = Kernel()
-    grid = np.linspace(0, 1, 16, endpoint=False)
-    y = np.sin(grid * 4 * np.pi)
-
+    X = np.linspace(0, 1, 16, endpoint=False)
+    y = np.sin(X * 4 * np.pi)
     mask = np.array([1, 0, 1, 0, 1, 0, 1, 0,
                      0, 1, 0, 1, 0, 1, 0, 1], dtype=np.bool_)
 
     gpr = GaussianProcessRegressor(kernel=kernel, alpha=1e-10)
-
-    gpr.fit(grid[mask], y[mask])
-    z = gpr.predict(grid[~mask])
+    gpr.fit(X[mask], y[mask])
+    z = gpr.predict(X[~mask])
     assert(z == pytest.approx(y[~mask], 1e-6))
+
+
+def test_gpr_hyperparameter_optimization():
+    '''test with a function with exactly two periods, and see if the GPR
+    can identify the frequency via hyperparameter optimization.'''
+
+    class Kernel:
+        def __init__(self, p, L):
+            self.p = p
+            self.L = L
+
+        def __call__(self, X, Y=None, eval_gradient=False):
+            d = np.subtract.outer(X, Y if Y is not None else X)
+            f = np.exp(-2 * np.sin(np.pi / self.p * d)**2 / self.L**2)
+            if eval_gradient is False:
+                return f
+            else:
+                s = np.sin(d * np.pi / self.p)
+                c = np.cos(d * np.pi / self.p)
+                j1 = 2.0 * np.pi * d * 2 * s * c * f / self.p**2 / self.L**2
+                j2 = 4.0 * s**2 * f / self.L**3
+                return f, np.array([j1, j2])
+
+        def diag(self, X):
+            return np.ones_like(X)
+
+        @property
+        def theta(self):
+            return np.log([self.p, self.L])
+
+        @theta.setter
+        def theta(self, t):
+            self.p, self.L = np.exp(t)
+
+        @property
+        def bounds(self):
+            return np.log([[1e-2, 10], [1e-2, 10]])
+
+    X = np.linspace(0, 1, 16, endpoint=False)
+    y = np.sin(X * 4 * np.pi)
+    kernel = Kernel(0.49, 0.1)
+    gpr = GaussianProcessRegressor(kernel=kernel, alpha=1e-10, optimizer=True)
+    gpr.fit(X, y)
+    assert(kernel.p == pytest.approx(0.5, 1e-2))
