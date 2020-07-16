9be5924a0ba2a1bae209f87e701f2863ea781fa0
Yu-Hang "Maxin" Tang <Tang.Maxin@gmail.com>
2020-07-16 11:27:44 -0700

    Since `VolatileCookie` is a subclass of built-in type `dict`, to customize its pickle serializaiton, we must define `__reduce__` instead of `__getstate__`.

--- graphdot/util/cookie.py
+++ graphdot/util/cookie.py
@@ -4,11 +4,8 @@
 
 class VolatileCookie(dict):
 
-    def __getstate__(self):
-        return None
-
-    def __setstate__(self, state):
-        pass
+    def __reduce__(self):
+        return (VolatileCookie.__new__, (VolatileCookie,))
 
     def __deepcopy__(self, memo):
         '''Deep copy of a volatile cookie is intentionally nullified.'''
