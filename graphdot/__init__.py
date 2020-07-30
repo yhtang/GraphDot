347ea0b88879f86adb331945e8fc4a4de053cdcd
Yu-Hang "Maxin" Tang <Tang.Maxin@gmail.com>
2020-07-30 09:57:40 -0700

    assign temporary version number

--- graphdot/__init__.py
+++ graphdot/__init__.py
@@ -5,7 +5,7 @@ from .graph import Graph
 
 __all__ = ['Graph']
 
-__version__ = '0.6.1'
+__version__ = '0.6x'
 __author__ = 'Yu-Hang "Maxin" Tang, Oguz Selvitopi'
 __maintainer__ = 'Yu-Hang "Maxin" Tang'
 __email__ = 'Tang.Maxin@gmail.com'
