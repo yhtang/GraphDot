c18c9ef4a3d21c990abeb60050cb4a17797e9924
Yu-Hang "Maxin" Tang <Tang.Maxin@gmail.com>
2020-09-02 11:21:35 -0700

    Added a GP variance minimization active learner.

--- graphdot/model/active_learning/__init__.py
+++ graphdot/model/active_learning/__init__.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 from .hierarchical_drafter import HierarchicalDrafter
 from .determinant_maximizer import DeterminantMaximizer
+from .variance_minimizer import VarianceMinimizer
 
 
-__all__ = ['HierarchicalDrafter', 'DeterminantMaximizer']
+__all__ = ['HierarchicalDrafter', 'DeterminantMaximizer', 'VarianceMinimizer']
