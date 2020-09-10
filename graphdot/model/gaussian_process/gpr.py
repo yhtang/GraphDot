fb826f8586b30fa6a90144513c6801824cf733a3
Yu-Hang "Maxin" Tang <Tang.Maxin@gmail.com>
2020-09-10 09:55:01 -0700

    The 'clamp' mode is still better than the 'truncate' mode for pinvh.

--- graphdot/model/gaussian_process/gpr.py
+++ graphdot/model/gaussian_process/gpr.py
@@ -122,7 +122,9 @@ class GaussianProcessRegressor:
                 warnings.warn(
                     'Kernel matrix singular, falling back to pseudoinverse'
                 )
-                return pinvh(K, rcond=self.beta, return_nlogdet=True)
+                return pinvh(
+                    K, rcond=self.beta, mode='clamp', return_nlogdet=True
+                )
             except np.linalg.LinAlgError:
                 raise np.linalg.LinAlgError(
                     'The kernel matrix is likely corrupted with NaNs and Infs '
