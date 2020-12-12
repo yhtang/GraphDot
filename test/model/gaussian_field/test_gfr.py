c0d9af07324fee530a6ab2fb9c900ceccf006716
Yu-Hang "Maxin" Tang <Tang.Maxin@gmail.com>
2020-12-12 09:19:08 -0800

    Update unit test.

--- test/model/gaussian_field/test_gfr.py
+++ test/model/gaussian_field/test_gfr.py
@@ -8,428 +8,168 @@ from graphdot.model.gaussian_field import GaussianFieldRegressor
 from graphdot.model.gaussian_field import Weight
 
 
-def test_gaussian_field_fit_and_predict_before_set():
-    """
-    Tests that set_model must be called before fitting.
-    """
-    g = GaussianFieldRegressor(pairwise_distances)
-    X = np.array([1])
-    y = np.array([1])
-    with pytest.raises(Exception):
-        g.fit(X, y)
-    with pytest.raises(Exception):
-        g.predict(X, y)
+@pytest.mark.parametrize('case', [
+    # [A] --1.0-- [B] --1.0-- [C]
+    (
+        np.array([
+            [0.0, 1.0, 0.0],
+            [1.0, 0.0, 1.0],
+            [0.0, 1.0, 0.0],
+        ]),
+        [True, False, True],
+        lambda y, z: z[1] == pytest.approx(0.5 * (y[0] + y[2]), abs=1e-4)
+    ),
+    # [A] --3.0-- [B] --1.0-- [C]
+    (
+        np.array([
+            [0.0, 3.0, 0.0],
+            [3.0, 0.0, 1.0],
+            [0.0, 1.0, 0.0],
+        ]),
+        [True, False, True],
+        lambda y, z: z[1] == pytest.approx(0.75 * y[0] + 0.25 * y[2], abs=1e-4)
+    ),
+    # [A] --1.0-- [B] --1.0-- [C] --1.0-- [D]
+    (
+        np.array([
+            [0.0, 1.0, 0.0, 0.0],
+            [1.0, 0.0, 1.0, 0.0],
+            [0.0, 1.0, 0.0, 1.0],
+            [0.0, 0.0, 1.0, 0.0],
+        ]),
+        [True, False, False, True],
+        lambda y, z: (
+            z[1] == pytest.approx((2 * y[0] + 1 * y[-1]) / 3, abs=1e-4) and
+            z[2] == pytest.approx((1 * y[0] + 2 * y[-1]) / 3, abs=1e-4)
+        )
+    ),
+    # Fully connected
+    (
+        np.array([
+            [0.0, 1.0, 1.0, 1.0],
+            [1.0, 0.0, 1.0, 1.0],
+            [1.0, 1.0, 0.0, 1.0],
+            [1.0, 1.0, 1.0, 0.0],
+        ]),
+        [True, True, True, False],
+        lambda y, z: (
+            z[3] == pytest.approx((y[0] + y[1] + y[2]) / 3, abs=1e-4)
+        )
+    ),
+])
+def test_gaussian_field_prediction(case):
+    W, labeled, verify = case
 
-
-def test_gaussian_field():
-    """
-    Tests Gaussian Field Regressor Prediction
-    """
-    class Test_Weight():
-
+    class WeightLookUpTable:
         def __call__(self, X, Y=None):
             if Y is None:
-                return pairwise_distances(X, X)
+                return W[X, :][:, X]
             else:
-                return pairwise_distances(X, Y)
+                return W[X, :][:, Y]
 
-    X = np.array([
-            [1, 0],
-            [0, 1],
-            [-1, 0],
-            [0, -1],
-        ])
-    y = np.array([1, 1, 1, 0])
+    g = GaussianFieldRegressor(WeightLookUpTable(), smoothing=0)
 
-    g = GaussianFieldRegressor(Test_Weight(), smoothing=0)
-    g.set_model(X, y)
-
-    Xu = np.array([[0, 0]])
-    assert g.predict(Xu) == np.array([.75])
+    for _ in range(100):
+        X = np.arange(len(W))
+        y = np.random.randn(len(W))
+        y[~np.array(labeled)] = np.nan
+        z = g.fit_predict(X, y)
+        assert len(y) == len(z)
+        assert verify(y, z)
 
 
-def test_gaussian_field_prediction1():
-    """
-    Tests Gaussian Field Regressor Prediction
-    """
-    mock = MagicMock()
-    X = np.array([0, 1])
-    y = np.array([0, 1])
-    W_ul = np.array([
-            [1, 0],
-            [0, 0],
-            [0, 1],
-        ])
-    W_uu = np.array([
-            [0, 1, 0],
-            [1, 0, 1],
-            [0, 1, 0],
-        ])
-    Xt = np.array([0, 1, 2])
-    mock.side_effect = [W_ul, W_uu]
-    g = GaussianFieldRegressor(mock, smoothing=0)
-    g.set_model(X, y)
-    A = g.predict(Xt)
-    B = np.array([.25, .5, .75])
-    assert np.allclose(A, B)
+@pytest.mark.parametrize('case', [
+    # [A] --1.0-- [B] --1.0-- [C]
+    (
+        np.array([
+            [0.0, 1.0, 0.0],
+            [1.0, 0.0, 1.0],
+            [0.0, 1.0, 0.0],
+        ]),
+        [True, False, True],
+        [[0.5, 0.5]],
+    ),
+    # [A] --3.0-- [B] --1.0-- [C]
+    (
+        np.array([
+            [0.0, 3.0, 0.0],
+            [3.0, 0.0, 1.0],
+            [0.0, 1.0, 0.0],
+        ]),
+        [True, False, True],
+        [[0.75, 0.25]]
+    ),
+    # [A] --1.0-- [B] --1.0-- [C] --1.0-- [D]
+    (
+        np.array([
+            [0.0, 1.0, 0.0, 0.0],
+            [1.0, 0.0, 1.0, 0.0],
+            [0.0, 1.0, 0.0, 1.0],
+            [0.0, 0.0, 1.0, 0.0],
+        ]),
+        [True, False, False, True],
+        [[2/3, 1/3],
+         [1/3, 2/3]]
+    ),
+    # Fully connected
+    (
+        np.array([
+            [0.0, 1.0, 1.0, 1.0],
+            [1.0, 0.0, 1.0, 1.0],
+            [1.0, 1.0, 0.0, 1.0],
+            [1.0, 1.0, 1.0, 0.0],
+        ]),
+        [True, True, True, False],
+        [[1/3, 1/3, 1/3]]
+    ),
+])
+def test_gaussian_field_influence(case):
+    W, labeled, truth = case
 
-
-def test_gaussian_field_prediction2():
-    """
-    Tests Gaussian Field Regressor Prediction
-    """
-    mock = MagicMock()
-    X = np.array([0])
-    y = np.array([1])
-    W_ul = np.array([
-            [1],
-            [1],
-            [1],
-            [1],
-        ])
-    W_uu = np.array([
-            [0, 1, 1, 1],
-            [1, 0, 1, 1],
-            [1, 1, 0, 1],
-            [1, 1, 1, 0],
-        ])
-    Xt = np.array([1, 2, 3, 4])
-    mock.side_effect = [W_ul, W_uu]
-    g = GaussianFieldRegressor(mock, smoothing=0)
-    g.set_model(X, y)
-    A = g.predict(Xt)
-    B = np.array([1, 1, 1, 1])
-    assert np.allclose(A, B)
-
-
-def test_smoothing():
-    """
-    Tests Gaussian Field Regressor Prediction
-    """
-    mock = MagicMock()
-    X = np.array([0, 1])
-    y = np.array([0, 1])
-    W_ul = np.array([
-            [1, 0],
-            [0, 0],
-            [0, 1],
-        ])
-    W_uu = np.array([
-            [0, 1, 0],
-            [1, 0, 1],
-            [0, 1, 0],
-        ])
-    Xt = np.array([0, 1, 2])
-    mock.side_effect = [W_ul, W_uu]
-    g = GaussianFieldRegressor(mock, smoothing=1e-2)
-    g.set_model(X, y)
-    A = g.predict(Xt)
-    B = np.array([.2525, .5, .7475])
-    assert np.allclose(A, B)
-
-
-def test_gaussian_field_influence_matrix():
-    """
-    Tests Gaussian Field Regressor Prediction
-    """
-    mock = MagicMock()
-    X = np.array([0])
-    y = np.array([1])
-    W_ul = np.array([
-            [1],
-            [1],
-            [1],
-            [1],
-        ])
-    W_uu = np.array([
-            [0, 1, 1, 1],
-            [1, 0, 1, 1],
-            [1, 1, 0, 1],
-            [1, 1, 1, 0],
-        ])
-    Xt = np.array([1, 2, 3, 4])
-    mock.side_effect = [W_ul, W_uu]
-    g = GaussianFieldRegressor(mock, smoothing=0)
-    g.set_model(X, y)
-    _, A, _ = g.predict(Xt, display=True)
-    B = np.array([1, 1, 1, 1])
-    assert np.allclose(np.dot(A, y), B)
-
-
-def test_squared_error_gradient():
-
-    class Test_Weight(Weight):
-        def __init__(self, sigma, bounds):
-            self.sigma = sigma
-            self._bounds = bounds
-
-        def __call__(self, X, Y=None, eval_gradient=False):
+    class WeightLookUpTable:
+        def __call__(self, X, Y=None):
             if Y is None:
-                Y = X
-            m = pairwise_distances(X, Y)
-            w = np.exp(-(m/self.sigma[0])**2/2)
-            if eval_gradient:
-                return w, np.array([m**2/self.sigma[0]**3]) * w
+                return W[X, :][:, X]
             else:
-                return w
+                return W[X, :][:, Y]
 
-        @property
-        def theta(self):
-            return self.sigma
+    g = GaussianFieldRegressor(WeightLookUpTable(), smoothing=0)
 
-        @theta.setter
-        def theta(self, values):
-            self.sigma = values
+    X = np.arange(len(W))
+    y = np.random.randn(len(W))
+    y[~np.array(labeled)] = np.nan
+    z, influence = g.fit_predict(X, y, return_influence=True)
+    assert np.allclose(influence, truth)
 
-        @property
-        def bounds(self):
-            return self._bounds
 
-    np.random.seed(0)
-    X_train = np.random.rand(20, 2)
-    y_train = np.random.rand(20)
-    X_test = np.random.rand(5, 2)
-    y_test = np.random.rand(5)
+def test_average_label_entropy():
 
-    sigma = np.array([.5])
-    bounds = np.array([[.3, 1]])
-    eps = 1e-3
-    g = GaussianFieldRegressor(
-        Test_Weight(sigma, bounds),
-        optimizer=True,
-        smoothing=0,
+    g = GaussianFieldRegressor(weight='precomputed', smoothing=0)
+
+    e = g.average_label_entropy(
+        X=np.array([
+            [0.0, 1.0, 0.0],
+            [1.0, 0.0, 1.0],
+            [0.0, 1.0, 0.0],
+        ]),
+        y=np.array([0, np.nan, 1])
     )
-    g.set_model(X_train, y_train)
-    _, grad = g.squared_error(X_test, y_test, eval_gradient=True)
 
-    g.weight.theta += eps
-    f1 = g.squared_error(X_test, y_test)
-    g.weight.theta -= 2 * eps
-    f2 = g.squared_error(X_test, y_test)
-    g.weight.theta += eps
-    assert np.isclose((f1 - f2)/(2 * eps), grad[0])
+    assert e == pytest.approx(-np.log(0.5))
 
 
-def test_cross_entropy_gradient():
+def test_laplacian():
 
-    class Test_Weight(Weight):
-        def __init__(self, sigma, bounds):
-            self.sigma = sigma
-            self._bounds = bounds
+    g = GaussianFieldRegressor(weight='precomputed', smoothing=0)
 
-        def __call__(self, X, Y=None, eval_gradient=False):
-            if Y is None:
-                Y = X
-            m = pairwise_distances(X, Y)
-            w = np.exp(-(m/self.sigma[0])**2/2)
-            if eval_gradient:
-                return w, np.array([m**2/self.sigma[0]**3]) * w
-            else:
-                return w
-
-        @property
-        def theta(self):
-            return self.sigma
-
-        @theta.setter
-        def theta(self, values):
-            self.sigma = values
-
-        @property
-        def bounds(self):
-            return self._bounds
-
-    np.random.seed(0)
-    X_train = np.random.rand(20, 2)
-    y_train = np.random.rand(20)
-    X_test = np.random.rand(5, 2)
-    y_test = np.random.rand(5)
-
-    sigma = np.array([.5])
-    bounds = np.array([[.3, 1]])
-    eps = 1e-3
-    g = GaussianFieldRegressor(
-        Test_Weight(sigma, bounds),
-        optimizer=True,
-        smoothing=0,
+    e = g.laplacian(
+        X=np.array([
+            [0.0, 1.0, 0.0],
+            [1.0, 0.0, 1.0],
+            [0.0, 1.0, 0.0],
+        ]),
+        y=np.zeros(3)
     )
-    g.set_model(X_train, y_train)
-    _, grad = g.cross_entropy(X_test, y_test, eval_gradient=True)
 
-    g.weight.theta += eps
-    f1 = g.cross_entropy(X_test, y_test)
-    g.weight.theta -= 2 * eps
-    f2 = g.cross_entropy(X_test, y_test)
-    g.weight.theta += eps
-    assert np.isclose((f1 - f2)/(2 * eps), grad[0])
+    assert e == pytest.approx(0)
 
-
-def test_laplacian_gradient():
-
-    class Test_Weight(Weight):
-        def __init__(self, sigma, bounds):
-            self.sigma = sigma
-            self._bounds = bounds
-
-        def __call__(self, X, Y=None, eval_gradient=False):
-            if Y is None:
-                Y = X
-            m = pairwise_distances(X, Y)
-            w = np.exp(-(m/self.sigma[0])**2/2)
-            if eval_gradient:
-                return w, np.array([m**2/self.sigma[0]**3]) * w
-            else:
-                return w
-
-        @property
-        def theta(self):
-            return self.sigma
-
-        @theta.setter
-        def theta(self, values):
-            self.sigma = values
-
-        @property
-        def bounds(self):
-            return self._bounds
-
-    np.random.seed(0)
-    X_train = np.random.rand(20, 2)
-    y_train = np.random.rand(20)
-
-    sigma = np.array([.5])
-    bounds = np.array([[.3, 1]])
-    eps = 1e-3
-    g = GaussianFieldRegressor(
-        Test_Weight(sigma, bounds),
-        optimizer=True,
-        smoothing=0,
-    )
-    g.set_model(X_train, y_train)
-    _, grad = g.laplacian_error(eval_gradient=True)
-
-    g.weight.theta += eps
-    f1 = g.laplacian_error()
-    g.weight.theta -= 2 * eps
-    f2 = g.laplacian_error()
-    g.weight.theta += eps
-
-    assert np.isclose((f1 - f2)/(2 * eps), grad[0])
-
-
-def test_average_label_entropy_gradient():
-
-    class Test_Weight(Weight):
-        def __init__(self, sigma, bounds):
-            self.sigma = sigma
-            self._bounds = bounds
-
-        def __call__(self, X, Y=None, eval_gradient=False):
-            if Y is None:
-                Y = X
-            m = pairwise_distances(X, Y)
-            w = np.exp(-(m/self.sigma[0])**2/2)
-            if eval_gradient:
-                return w, np.array([m**2/self.sigma[0]**3]) * w
-            else:
-                return w
-
-        @property
-        def theta(self):
-            return self.sigma
-
-        @theta.setter
-        def theta(self, values):
-            self.sigma = values
-
-        @property
-        def bounds(self):
-            return self._bounds
-
-    np.random.seed(0)
-    X_train = np.random.rand(20, 2)
-    y_train = np.random.rand(20)
-    X_test = np.random.rand(5, 2)
-    y_test = np.random.rand(5)
-
-    sigma = np.array([.5])
-    bounds = np.array([[.3, 1]])
-    eps = 1e-3
-    g = GaussianFieldRegressor(
-        Test_Weight(sigma, bounds),
-        optimizer=True,
-        smoothing=0,
-    )
-    g.set_model(X_train, y_train)
-    _, grad = g.average_label_entropy(X_test, y_test, eval_gradient=True)
-
-    g.weight.theta += eps
-    f1 = g.average_label_entropy(X_test, y_test)
-    g.weight.theta -= 2 * eps
-    f2 = g.average_label_entropy(X_test, y_test)
-    g.weight.theta += eps
-
-    assert np.isclose((f1 - f2)/(2 * eps), grad[0])
-
-
-def test_eta():
-    mock = MagicMock()
-    W = np.array([
-            [0, 1, 0.1],
-            [1, 0, 0.2],
-            [0.1, 0.2, 0],
-        ])
-    f_l = [1, 2]
-    mock.side_effect = [W]
-    g = GaussianFieldRegressor(mock, smoothing=0, eta=0.5)
-    g.set_model(np.zeros((2, 2)), f_l)
-    x = g.predict(np.array([[0, 0]]))
-    assert np.allclose(x, np.array([1.3392226, 1.6890459, 1.5724382]))
-
-
-def test_fit():
-
-    class Test_Weight(Weight):
-        def __init__(self, sigma, bounds):
-            self.sigma = sigma
-            self._bounds = bounds
-
-        def __call__(self, X, Y=None, eval_gradient=False):
-            if Y is None:
-                Y = X
-            m = pairwise_distances(X, Y)
-            w = np.exp(-(m/self.sigma[0])**2/2)
-            if eval_gradient:
-                return w, np.array([m**2/self.sigma[0]**3]) * w
-            else:
-                return w
-
-        @property
-        def theta(self):
-            return self.sigma
-
-        @theta.setter
-        def theta(self, values):
-            self.sigma = values
-
-        @property
-        def bounds(self):
-            return self._bounds
-
-    np.random.seed(0)
-    X_train = np.random.rand(20, 2)
-    y_train = np.random.rand(20)
-    X_test = np.random.rand(5, 2)
-    y_test = np.random.rand(5)
-
-    sigma = np.array([1])
-
-    bounds = np.array([[1, 10]])
-    g = GaussianFieldRegressor(
-        Test_Weight(sigma, bounds),
-        optimizer=True,
-        smoothing=0,
-    )
-    g.set_model(X_train, y_train)
-    g.fit(Z=X_test, y=y_test)
-    assert g.weight.theta == np.array([10])
