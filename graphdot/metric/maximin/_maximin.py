Fixed a bug due to incorrect diagonal gradient layout.

graphdot/metric/maximin/_maximin.py
@@ -122,8 +122,15 @@ class MaxiMin(MarginalizedGraphKernel):
                 self.n_dims * int(np.prod(output_shape)), np.float32
             )
             r, dr = diag
-            diags = [backend.array(np.r_[r[b:e], dr[b:e, :].ravel()])
+            print(f'r\n{r}')
+            print(f'dr\n{dr}')
+            diags = [backend.array(np.r_[r[b:e], dr[b:e, :].ravel('F')])
                      for b, e in zip(starts_nodal[:-1], starts_nodal[1:])]
+            print('=====================')
+            print('diags')
+            for d in diags:
+                print(d)
+                print('=====================')
         else:
             gradient = None
             diags = [backend.array(diag[b:e])